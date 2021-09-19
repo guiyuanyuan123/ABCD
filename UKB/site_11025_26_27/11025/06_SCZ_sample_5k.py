b43486e3a563f6854bd5d6d6c6e37da29a651a0f
guiyuanyuan123 <60128401+guiyuanyuan123@users.noreply.github.com>
2021-09-19 20:07:00 +0800

    Update 06_SCZ_sample_5k.py

--- UKB/site_11025_26_27/11025/06_SCZ_sample_5k.py
+++ UKB/site_11025_26_27/11025/06_SCZ_sample_5k.py
@@ -6,4 +6,6 @@ for i in range(n):
     data['SCORE_SCZ'+str(i)] = np.random.choice(data['SCORE_SCZ'].values,data.shape[0],replace=False)
 
 np.savetxt("/export/home/guiyuanyuan/UKB/brain_imaging/notask_fmri/site_54/11025_Cheadle/SCZ20/SCZ_random_profile_5k",data,fmt='%s')
-#cat ~/measures/SDPR/SCZ/scz20/3 SCZ_short_profile_5k > SCZ_20_short_profile_5k
+#for i in `cat test`;do cp SCZ_header ${i}_header;sed -i 's/SCZ/'${i}'/g' ${i}_header;done
+#for i in `cat test`;do cat ${i}_header ${i}20/${i}_random_profile_5k > ${i}20/${i}20_random_profile_5k;cat ${i}_header ${i}20/${i}_random_profile_1w > ${i}20/${i}20_random_profile_1w;done
+#for i in `cat test`;do rm ${i}20/${i}_random_profile_5k ${i}20/${i}_random_profile_1w;done
